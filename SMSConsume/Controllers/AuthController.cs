f8f2ec232d16489f1352ef490d31c8e4f9525780
Shubham-Parad <shubhamparad19@gmail.com>
2024-09-10 17:11:09 +0530

    first commit

---
+++ SMSConsume/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMSConsume.Controllers
+{
+    public class AuthController : Controller
+    {
+        HttpClient client;
+        public AuthController()
+        {
+
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            client = new HttpClient(clientHandler);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
