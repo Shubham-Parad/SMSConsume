84b4f488de47de366bebf2bdd34f4d45cb3099fd
Shubham-Parad <shubhamparad19@gmail.com>
2024-09-12 17:23:17 +0530

    made few changes

--- SMSConsume/Controllers/AuthController.cs
+++ SMSConsume/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SMSConsume.Models;
+using System.Text;
 
 namespace SMSConsume.Controllers
 {
@@ -7,7 +10,6 @@ namespace SMSConsume.Controllers
         HttpClient client;
         public AuthController()
         {
-
             HttpClientHandler clientHandler = new HttpClientHandler();
             clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
             client = new HttpClient(clientHandler);
@@ -16,7 +18,47 @@ namespace SMSConsume.Controllers
         {
             return View();
         }
-        public IActionResult SignIn()
+        public IActionResult SignIn(Users u)
+        {
+            string url = "https://localhost:7186/api/Auth/SignIn/";
+            var jsondata = JsonConvert.SerializeObject(u);
+            StringContent stringContent = new StringContent(jsondata, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(url, stringContent).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                if (u.Urole == "Admin")
+                {
+                    return RedirectToAction("Index");
+                }
+                else if (u.Urole == "Student")
+                {
+                    return RedirectToAction("Index");
+                }
+                else if (u.Urole == "Teacher")
+                {
+                    return RedirectToAction("Index");
+                }
+                else if (u.Urole == "Parent")
+                {
+                    return RedirectToAction("Index");
+                }
+                else if (u.Urole == "Accountant")
+                {
+                    return RedirectToAction("Index");
+                }
+                else if (u.Urole == "Librarian")
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+
+        public IActionResult SignUp()
         {
             return View();
         }
